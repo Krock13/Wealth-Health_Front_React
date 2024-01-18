bcc0cc6934da41d67ce36640d584e1a37be47fbc
krock13 <krock.airdrop@hotmail.com>
2024-01-18 17:30:13 +0100

    Change 'NotFound' page to English

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -4,12 +4,12 @@ import styles from './notFound.module.css';
 export function NotFound() {
   return (
     <div className={styles.notFoundContainer}>
-      <h1 className={styles.notFoundTitle}>Oups ! Page non trouvée</h1>
+      <h1 className={styles.notFoundTitle}>Oops! Page not found</h1>
       <p className={styles.notFoundDescription}>
-        Désolé, la page que vous cherchez n&apos;existe pas ou a été déplacée.
+        Sorry, the page you are looking for does not exist or has been moved.
       </p>
       <Link to='/' className={styles.notFoundLink}>
-        Retourner à la page d&apos;accueil
+        Back to home page
       </Link>
     </div>
   );
